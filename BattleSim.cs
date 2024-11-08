93bf4dc8fe5e4b8105273024fc64cf1d73a6dbbd
TacoBoi6 <noahstacey1006@gmail.com>
2024-11-08 14:36:39 -0500

    mostly just Pokemon.ToString() stuff

---
+++ BattleSim.cs
@@ -0,0 +1,12 @@
+﻿using System; 
+public static class BattleSim { 
+    public static void Main() { 
+        Pokemon venusaur = new Pokemon("Venusaur", new int[] {80, 82, 83, 100, 100, 80}, 100, new string[] {"grass", "poison"}); 
+        Pokemon charizard = new Pokemon("Charizard", new int[] {78, 84, 78, 109, 85, 100}, 100, new string[] {"fire", "flying"}); 
+        Pokemon blastoise = new Pokemon("Blastoise", new int[] {79, 83, 100, 85, 105, 78}, 100, new string[] {"water", "none"}); 
+
+        Console.WriteLine(venusaur + "\n"); 
+        Console.WriteLine(charizard + "\n"); 
+        Console.WriteLine(blastoise + "\n"); 
+    }
+}
--- Pokemon.cs
+++ Pokemon.cs
@@ -4,8 +4,9 @@ public class Pokemon {
     int[] ivs = new int[6]; 
     int[] evs = new int[6]; 
     String[] type = new string[2]; 
-    String nature; 
+    //String nature; 
     int level; 
+    // should add nickname 
 
     // new pokemon with no info  
     public Pokemon() { 
@@ -14,35 +15,48 @@ public class Pokemon {
         ivs = new int[] {0, 0, 0, 0, 0, 0}; 
         evs = new int[] {0, 0, 0, 0, 0, 0}; 
         level = 1; 
+        type[0] = "none"; 
         type[1] = "none"; 
-        type[2] = "none"; 
-        nature = "serious"; 
+        //nature = "serious"; 
     }
 
-    // new pokemon with name, base stats, level, typing, nature 
-    public Pokemon(String n, int[] bS, int lvl, String[] t, String nat) { 
+    // new pokemon with name, base stats, level, typing 
+    public Pokemon(String n, int[] bS, int lvl, String[] t) { 
         name = n; 
         for (int i = 0; i < 6; i++) 
             baseStats[i] = bS[i]; 
         level = lvl; 
-        type[1] = t[1];
-        type[2] = t[2];  
-        nature = nat; 
+        type[0] = t[0];
+        type[1] = t[1];  
+        //nature = nat; 
     }
 
     // string representation 
     public override String ToString() { 
+        // add species name 
         String str = ""; 
-        str += name + "\n"; 
+        str += name; 
 
+        // add types 
+        str += "\tType: " + (type[1] == "none" ? Capitalize(type[0]) : Capitalize(type[0]) + "\\" + Capitalize(type[1])) + "\n"; 
+
+        // add stats 
         str += "HP\tAtk\tDef\tSpAtk\tSpDef\tSpe\n"; 
         foreach (int statVal in baseStats) 
             str += statVal + "\t"; 
         
-
-
         return str; 
 
-        // should add evo line and stage 
+        // should add evo line and evo stage 
+    }
+
+    // capitalize the first letter of a string
+    //      i dont really know what class to put this in tbh 
+    //      i want it to be able to be called on a String like .Substring() but idk how to access that class 
+    //      (or if im even supposed to) 
+    public static String Capitalize(String str) { 
+        str = Char.ToUpper(str[0]) + str.Substring(1, str.Length - 1); 
+        
+        return str; 
     }
 } 
